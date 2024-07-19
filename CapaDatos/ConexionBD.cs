11996642b50303a8c2ac31469bac2e601cbb6740
SerDev <108685183+SergioMVN@users.noreply.github.com>
2024-07-19 01:25:23 -0400

    se configuro cadena de conexion a base de datos

---
+++ CapaDatos/ConexionBD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ConexionBD
+    {
+
+        private string ObtenerCadenaDeConexion()
+        {
+            return ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+        }
+
+        public void EjecutarConsulta()
+        {
+            string cadenaDeConexion = ObtenerCadenaDeConexion();
+            using (SqlConnection connection = new SqlConnection(cadenaDeConexion))
+            {
+                // Tu código para interactuar con la base de datos
+            }
+        }
+    }
+}
